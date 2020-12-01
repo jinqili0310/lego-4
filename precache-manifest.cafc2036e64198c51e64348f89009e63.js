b0aa2331d2978100ab97e80febc3307b4bc7493c
Jinqi Li <jxl2047@miami.edu>
2020-12-01 03:00:51 -0500

    Updates

---
+++ precache-manifest.cafc2036e64198c51e64348f89009e63.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb494c1b88fa7744f63cfd8374981dac",
+    "url": "/lego-4/index.html"
+  },
+  {
+    "revision": "15aaf820257a9a0e9b10",
+    "url": "/lego-4/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d36bb64f0bc68b855be9",
+    "url": "/lego-4/static/css/main.9cb4ddc9.chunk.css"
+  },
+  {
+    "revision": "15aaf820257a9a0e9b10",
+    "url": "/lego-4/static/js/2.567b5293.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/lego-4/static/js/2.567b5293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d36bb64f0bc68b855be9",
+    "url": "/lego-4/static/js/main.59399b3a.chunk.js"
+  },
+  {
+    "revision": "1c71ad78022e91dc5fed",
+    "url": "/lego-4/static/js/runtime-main.273f01ba.js"
+  },
+  {
+    "revision": "1c1d14cc1cce71d58ccec0dbeec5e610",
+    "url": "/lego-4/static/media/21322.1c1d14cc.webp"
+  },
+  {
+    "revision": "a677af49de35a7164c4969e226fd7d7b",
+    "url": "/lego-4/static/media/31199-Zebra-Marvel-Homepage--072320--Block-Standard-Lifestyle.a677af49.webp"
+  },
+  {
+    "revision": "28dc1c20d68f4c8c721511cfa59b09e4",
+    "url": "/lego-4/static/media/31199.28dc1c20.webp"
+  },
+  {
+    "revision": "f5c6c55d2e8bdcdf70fc05645b391b7a",
+    "url": "/lego-4/static/media/40428-HP-2020-Block-Standard.f5c6c55d.webp"
+  },
+  {
+    "revision": "c6838e8f699b81b4d563653dfbc47e52",
+    "url": "/lego-4/static/media/43179-Disney-Characters-202006-Homepage-Still-Life-2-Block-Standard.c6838e8f.webp"
+  },
+  {
+    "revision": "b7efc7f825dfdfabe2bb4826ca422098",
+    "url": "/lego-4/static/media/71360-Homepage-202008-Hero-Standard-Large.b7efc7f8.webp"
+  },
+  {
+    "revision": "63d7fb50a2a5504fbe3a35dfc9ea1355",
+    "url": "/lego-4/static/media/71361.63d7fb50.webp"
+  },
+  {
+    "revision": "5e4be0653c913f2c6be413f94f12bb5d",
+    "url": "/lego-4/static/media/71367.5e4be065.webp"
+  },
+  {
+    "revision": "572c609aef9aa5ef7d1b096b866d0c95",
+    "url": "/lego-4/static/media/71369.572c609a.webp"
+  },
+  {
+    "revision": "e572c59783bf378ef189c33db6753fcd",
+    "url": "/lego-4/static/media/71377.e572c597.webp"
+  },
+  {
+    "revision": "a665b38f2fae34ea4c88e205e7f14c6b",
+    "url": "/lego-4/static/media/75941-Home-202006-Block-Standard.a665b38f.webp"
+  },
+  {
+    "revision": "91bec623ee4858db845dd5744fea121c",
+    "url": "/lego-4/static/media/76153-HP-202006-Block-Standard.91bec623.webp"
+  },
+  {
+    "revision": "a26c9782ee62b44ec0dbea5d0b8b426b",
+    "url": "/lego-4/static/media/Cera Pro Black Italic.a26c9782.otf"
+  },
+  {
+    "revision": "95d55d992d7f63799ad116dbd12a25cf",
+    "url": "/lego-4/static/media/Cera Pro Black.95d55d99.otf"
+  },
+  {
+    "revision": "07ac815392d30140366e0a9df7f769e8",
+    "url": "/lego-4/static/media/Cera Pro Bold.07ac8153.otf"
+  },
+  {
+    "revision": "112299d7917b38aff426d451d495f3d7",
+    "url": "/lego-4/static/media/Cera Pro Light.112299d7.otf"
+  },
+  {
+    "revision": "afe6902a46c9d424e9aca916f97562db",
+    "url": "/lego-4/static/media/Cera Pro Medium.afe6902a.otf"
+  },
+  {
+    "revision": "0b7b37be489a128959fe71c97850e08f",
+    "url": "/lego-4/static/media/Cera Pro Regular Italic.0b7b37be.otf"
+  },
+  {
+    "revision": "020748a647888804f56a3662e48f21bd",
+    "url": "/lego-4/static/media/LEGO-Logo-Ani2b.020748a6.gif"
+  }
+]);
---
+++ scripts/Leo_NoP.js
@@ -0,0 +1,299 @@
+var icon_img = 'https://kikijinqili.github.io/assets/lego/LEGO-Logo-Ani2b.gif'
+var playAd = 0;
+var bye = 0;
+var sayBye// = setInterval(sayGoodbye, 600000);
+var user_name = '';
+var type_anim_on = 0;
+
+var prescripted_questions = new Array();
+var prescripted_mode = 0;
+var cur_question_index = 0;
+var bot_typing = 0;
+var correct_answer='';
+
+
+//bot-initiated dialogue
+class Question {
+	constructor(text, answers, next_questions, correct_answer_index) {
+		this.text = text;
+		this.answers = answers;
+		this.next_questions = next_questions;
+		this.correct_answer_index = correct_answer_index;
+	}
+}
+
+function createQuestions() {
+
+	prescripted_questions[0] = new Question(['How are you?'], ['Absolutely 😃', 'Eh, it’s alright 😶','Not really 😣'], 1, 1);
+	prescripted_questions[1] = new Question(["Let me cheer you up by showing you some of our beautiful LEGO sets"], [""], 2, 0);
+	prescripted_questions[2] = new Question(["https://kikijinqili.github.io/assets/lego/LEGO%20GIF4.gif"], [""], 3, 0);
+	prescripted_questions[3] = new Question(["Do you like it?"], ["Yes", "It's okay."], 4, 0);
+	prescripted_questions[4] = new Question(["Great. What can I help you with today?"], ["Products 🎪", "Orders 💻", "Learn About LEGO 💡", "Support 📩"], 5, 2);
+	prescripted_questions[5] = new Question(["Sounds good. What do you want to know?"], ["Our Commitment to Creativity 🎨", "Our History 📍", "The LEGO Foundation 💞", "LEGO Education 📚"], 6, 0);
+	prescripted_questions[6] = new Question(["Through our Lego Replay program, you can donate your used Lego bricks and we will send them to classrooms across the country to reach more creative young minds!"], [""], 7, 0);
+	prescripted_questions[7] = new Question(["How does that sound?"], ["Cool!", "I want to know more!"], 8, 0);
+	prescripted_questions[8] = new Question(["How else can I help you today? "], ["Learn More About LEGO 💡", "Explore The Products 🎪", "View Your Orders 💻", "Fun for Fans 🎮"], 9, 1);
+	prescripted_questions[9] = new Question(["Sounds good. What kind of products can I help you find?"], ["Electronic Products 📱", "Brick Sets 🔨"], 10, 1);
+	prescripted_questions[10] = new Question(["Which category do you want to explore?"], ["New ✨", "Popular 🔥", "Age 👶", "Theme 🚀"], 11, 1);
+	prescripted_questions[11] = new Question(["Are you interested in any of these brick sets?"], ["Jungle Rescue 🐘", "Friends Central Perk ☕", "Architecture Sets 🗽", "Monster Truck 🚛"], 12, 2);
+	prescripted_questions[12] = new Question(["Is this for you or someone you care about? 😉"], ["Only for me", "For someone else"], 13, 1);
+	prescripted_questions[13] = new Question(["Got it. How else can I help?"], ["I’m all set. 😆", "I’d like more help."], -1, 0);
+
+}
+
+function askNextQuestion(answer_text) {
+	//var pretext_needed = [4, 8];
+	var questions = prescripted_questions[cur_question_index].text;
+	var answer = prescripted_questions[cur_question_index].answers; // one number as index
+	var next_question = prescripted_questions[cur_question_index].next_questions; //one number as index
+	var correct_index = prescripted_questions[cur_question_index].correct_answer_index;
+
+	if(answer_text == answer[correct_index]) {
+		if(next_question > -1) {
+			bot_typing = 1;
+			var next_question_text = prescripted_questions[next_question].text;
+			var text = next_question_text[0]; //only one question for this script
+			var delay_time = 0;
+			var delay_img = 7000;
+
+			if(text.indexOf('gif') > -1) {
+				//post gif
+				image = text;
+				postImage(image);
+				delay_img = 1000;
+				//console.log(image);
+				/*setTimeout(function() {
+					postImage(image);
+				}, 5000);*/
+			} else {
+				//waitForBot = setInterval(typingAnim, 500);
+				
+				/*setTimeout(function() {
+					postBotAnswer(text);
+				}, 2000);*/
+				var post_text = text;
+
+				/*if (pretext_needed.includes(cur_question_index)) {
+					post_text = pretext + post_text;
+				}*/
+				//console.log(i);
+				//console.log(post_text);
+
+				//postBotAnswer(post_text);
+
+				// create node here to have typing animation
+				var newNode = document.createElement("div");
+		    	newNode.setAttribute("class", "chat_message_bot");
+		    	var newImg = document.createElement("img");
+		    	newImg.src = icon_img;
+		    	newImg.setAttribute("width", "8%");
+		    	newNode.appendChild(newImg);
+		    	newNode.appendChild(document.createTextNode("  Bot is typing..."));
+		    	var spaceHolderNode = document.getElementById("test");
+		    	
+
+		    	setTimeout(function() {
+		    		document.getElementById("chat_box").insertBefore(newNode, spaceHolderNode);
+		    		document.getElementById("chat_box").scrollTop = document.getElementById("chat_box").scrollHeight;
+				}, 2000);
+
+
+				if (text.split(' ').length > 27) { //long sentence
+					delay_time = 6000;
+					delay_img = 10000;
+					setTimeout(function() {
+						//postBotAnswer(post_text)
+						newNode.childNodes[1].nodeValue="  " + post_text;
+						var audio = new Audio('audio/Pling-KevanGC-1485374730.mp3');
+						audio.play();
+					}, 6000); //5000
+				} else {
+					delay_time = 4000;
+					setTimeout(function() {
+						//postBotAnswer(post_text)
+						newNode.childNodes[1].nodeValue="  " + post_text;
+						var audio = new Audio('audio/Pling-KevanGC-1485374730.mp3');
+						audio.play();
+					}, 4000);
+				}
+				setTimeout(function() {
+					document.getElementById("chat_box").scrollTop = document.getElementById("chat_box").scrollHeight
+				}, 6000);
+			}
+			
+			cur_question_index = next_question;	
+
+			if(prescripted_questions[cur_question_index].answers[0] == "") {
+				//clearInterval(waitForBot);
+				setTimeout(function() {
+					askNextQuestion("");
+				}, delay_img); //4000, 12000
+			} else {
+				setTimeout(function() {
+					createButtonQuickReply(prescripted_questions[cur_question_index].answers, cur_question_index);
+				}, delay_time);
+				bot_typing = 0;
+			}
+				
+
+		} else { //last closing chat
+			bye = 1;
+			setTimeout(function() {
+				var outstring = 'Okay. '+user_name+", if you need anything else, feel free to stop by and chat anytime.";
+				postBotAnswer(outstring);
+			}, 2000);
+
+			setTimeout(function() {
+				postImage('https://kikijinqili.github.io/assets/lego/LEGO%20GIF3.gif');
+			}, 4000);
+		}
+}
+	}
+
+function createButtonQuickReply(options, question_index) {
+   // options is a string array
+   
+	var newNode = document.createElement("div");
+	newNode.setAttribute("class", "chat_message_bot");
+	
+	options.forEach(function(answer) {
+		var button = document.createElement('button');
+		button.setAttribute('class', 'chat_button');
+		button.innerHTML = answer;
+		button.onclick = function() {
+			//console.log(answer);
+			//console.log(prescripted_questions[cur_question_index].answers[prescripted_questions[cur_question_index].correct_answer_index])
+			if(answer == prescripted_questions[cur_question_index].answers[prescripted_questions[cur_question_index].correct_answer_index]) {
+				document.getElementById("chat_box").removeChild(newNode);
+				document.getElementById("user_input").value = answer;
+				postUserInput();
+			}
+		};
+		newNode.appendChild(button);
+	});
+	var spaceHolderNode = document.getElementById("test");
+	document.getElementById("chat_box").insertBefore(newNode, spaceHolderNode);
+	document.getElementById("chat_box").scrollTop = document.getElementById("chat_box").scrollHeight;	
+}
+
+
+function postImage(img_filename) {
+	var newNode = document.createElement("div");
+	newNode.setAttribute("class", "chat_message_bot");
+	var newImg = document.createElement("img");
+	newImg.src = img_filename;
+	newImg.setAttribute("width", "60%");
+	//newImg.setAttribute("align", "middle");
+	newNode.appendChild(newImg);
+	newNode.setAttribute("align", "middle");
+	var spaceHolderNode = document.getElementById("test");
+	//var height = newImg.getAttribute("height");
+  	document.getElementById("chat_box").insertBefore(newNode, spaceHolderNode);
+   	//document.getElementById("chat_box").scrollTop = document.getElementById("chat_box").scrollHeight;
+   	setTimeout(function() {
+		document.getElementById("chat_box").scrollTop = document.getElementById("chat_box").scrollHeight
+	}, 500);
+}
+
+function postBotAnswer(text)
+{
+	//clearInterval(waitForBot);
+	
+	if (text!="") {
+		var newNode = document.createElement("div");
+    	newNode.setAttribute("class", "chat_message_bot");
+    	var newImg = document.createElement("img");
+    	newImg.src = icon_img;
+    	newImg.setAttribute("width", "8%");
+    	newNode.appendChild(newImg);
+    	newNode.appendChild(document.createTextNode(" " + text));
+    	var spaceHolderNode = document.getElementById("test");
+    	document.getElementById("chat_box").insertBefore(newNode, spaceHolderNode);
+    	document.getElementById("chat_box").scrollTop = document.getElementById("chat_box").scrollHeight;
+
+    	var audio = new Audio('audio/Pling-KevanGC-1485374730.mp3');
+		audio.play();
+	}
+	
+	setTimeout(function() {
+		document.getElementById("chat_box").scrollTop = document.getElementById("chat_box").scrollHeight
+	}, 1000);
+}
+
+function postUserInput() 
+{
+    var newNode = document.createElement("div");
+    newNode.setAttribute("class", "chat_message");
+    var text = document.getElementById("user_input").value;
+    
+    user_input = text.toLowerCase();
+    
+    var valid_input = false;
+    var wait_for_bot_response = false;
+
+
+	if(user_name == "") {
+		var input = text.split(" ");
+		user_name = input[input.length-1];
+		user_name = user_name.charAt(0).toUpperCase() + user_name.slice(1);
+
+
+		setTimeout(function() {
+			postBotAnswer("Hi, " + user_name + "!  👋Nice to meet you electronically 😊.")
+		}, 1000);
+
+		//ask the first question
+		setTimeout(function() {
+			postBotAnswer(prescripted_questions[cur_question_index].text);
+			createButtonQuickReply(prescripted_questions[cur_question_index].answers, 1);
+			prescripted_mode = 1;
+		}, 5000);
+	
+		valid_input = true;
+	
+	} else {
+		if(bye == 0 && bot_typing == 0) {
+			//if(text == correct_answer) {
+			//	valid_input = true;
+				setTimeout(function() {
+					//wait_for_bot_response = true;
+					askNextQuestion(text);
+				}, 500);
+		} 
+	}
+
+	//if(valid_input) {
+		newNode.appendChild(document.createTextNode(text));
+	    var spaceHolderNode = document.getElementById("test");
+	    //document.getElementById("chat_box").appendChild(newNode);
+	    document.getElementById("chat_box").insertBefore(newNode, spaceHolderNode);
+	    document.getElementById("chat_box").scrollTop = document.getElementById("chat_box").scrollHeight;
+	    document.getElementById("user_input").value = "";	
+	//}
+}
+
+function displayBot()
+{
+
+	document.getElementById("chat_box").style.display = "block";
+	document.getElementById("chat_input").style.display = "block";
+
+	
+	if(user_name == '') {
+		setTimeout(function() {
+		    postBotAnswer("Hi! I am LEGO’s chatbot powered by artificial intelligence! What’s your name?")
+		}, 500);
+	}
+}
+
+function start() 
+{
+    document.getElementById("chat_box").style.display = "none";
+    document.getElementById("chat_input").style.display = "none";
+    setTimeout(displayBot, 5000); // display chat box after 5 seconds
+
+    //pre-scripted questions
+    createQuestions();
+}
+
+window.addEventListener("load", start, false);
